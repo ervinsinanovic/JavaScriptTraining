c94791b8663a00447a0f20fa6af667fef9ad73f1
Ervin Sinanovic <ic22b019@technikum-wien.at>
2022-09-09 02:04:33 +0200

    test3

--- test1/index.js
+++ test1/index.js
@@ -1,5 +1,30 @@
 console.log("hello kind");
 
-var x =123
+var x = 123;
 
-console.log(x.toString())
+console.log(x.toString());
+
+function countingValleys(s) {
+  var count = 0;
+  var level = 0;
+  var currentInValley = false;
+  for (let i = 0; i <= s.length; i++) {
+    let direction = s.substring(i, i + 1);
+    if (direction.equals("U")) {
+      level++;
+      if (currentInValley == true && level >= 0) {
+        currentInValley = false;
+        count++;
+      }
+    } else {
+      if (direction.equals("D")) {
+        level--;
+        if (currentInValley == false && level < 0) {
+          currentInValley = true;
+        }
+      }
+    }
+  }
+}
+
+console.log(countingValleys("UFFDDFDUDFUFU"));
